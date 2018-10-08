e10ee64c672201c59c7762f8a89dfb6d8c27477b
FranciscoFrade <150221002@estudantes.ips.pt>
2018-10-08 10:12:02 +0100

    Add project files.

---
+++ VPHelloWorld_150221002/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VPHelloWorld_150221002.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
